2d84113a776a704892422599a471624ec41c2cb3
Risqi Romadhoni <risqir57@gmail.com>
2020-12-23 13:53:40 -0500

    modifi recent news

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -493,109 +493,40 @@
             <!--== Blog Content Wrapper ==-->
             <div class="row">
                 <!--== Single Blog Post start ==-->
-                <div class="col-lg-4 col-md-6">
-                    <article class="single-blog-post">
-                        <figure class="blog-thumb">
-                            <div class="blog-thumbnail">
-                                <img src="http://placehold.it/700x353" alt="Blog" class="img-fluid">
-                            </div>
-                            <figcaption class="blog-meta clearfix">
-                                <a href="single-blog.html" class="author">
-                                    <div class="author-pic">
-                                        <img src="http://placehold.it/200x200" alt="Author">
-                                    </div>
-                                    <div class="author-info">
-                                        <h5>Daney williams</h5>
-                                        <p>2 hours Ago</p>
-                                    </div>
-                                </a>
-                                <div class="like-comm pull-right">
-                                    <a href="#"><i class="fa fa-comment-o"></i>77</a>
-                                    <a href="#"><i class="fa fa-heart-o"></i>12</a>
+                @for ($i = 0; $i < 3; $i++)
+                    <div class="col-lg-4 col-md-6">
+                        <article class="single-blog-post">
+                            <figure class="blog-thumb">
+                                <div class="blog-thumbnail">
+                                    <img src="https://images.pexels.com/photos/159866/books-book-pages-read-literature-159866.jpeg" alt="Blog" class="img-fluid">
                                 </div>
-                            </figcaption>
-                        </figure>
+                                <figcaption class="blog-meta clearfix">
+                                    <a href="{{route('blog.show', ['detail'])}}" class="author">
+                                        <div class="author-pic">
+                                            <img src="{{asset('storage/users/default.png')}}" alt="Author">
+                                        </div>
+                                        <div class="author-info">
+                                            <h5>Daney williams</h5>
+                                            <p>2 hours Ago</p>
+                                        </div>
+                                    </a>
+                                    <div class="like-comm pull-right">
+                                        <a href="#"><i class="fa fa-comment-o"></i>77</a>
+                                        <a href="#"><i class="fa fa-heart-o"></i>12</a>
+                                    </div>
+                                </figcaption>
+                            </figure>
 
-                        <div class="blog-content">
-                            <h3><a href="single-blog.html">Recently we create a maassive project that</a></h3>
-                            <p>This is a big project of our company, we are happy to completed this type projec which
-                                are
-                                get world famous award</p>
-                            <a href="single-blog.html" class="btn btn-brand">More</a>
-                        </div>
-                    </article>
-                </div>
-                <!--== Single Blog Post End ==-->
-
-                <!--== Single Blog Post start ==-->
-                <div class="col-lg-4 col-md-6">
-                    <article class="single-blog-post">
-                        <figure class="blog-thumb">
-                            <div class="blog-thumbnail">
-                                <img src="http://placehold.it/700x352" alt="Blog" class="img-fluid">
+                            <div class="blog-content">
+                                <h3><a href="{{route('blog.show', ['detail'])}}">Recently we create a maassive project that</a></h3>
+                                <p>This is a big project of our company, we are happy to completed this type projec which
+                                    are
+                                    get world famous award</p>
+                                <a href="{{route('blog.show', ['detail'])}}" class="btn btn-brand">More</a>
                             </div>
-                            <figcaption class="blog-meta clearfix">
-                                <a href="single-blog.html" class="author">
-                                    <div class="author-pic">
-                                        <img src="http://placehold.it/200x200" alt="Author">
-                                    </div>
-                                    <div class="author-info">
-                                        <h5>Myra Hindley</h5>
-                                        <p>1 Day Ago</p>
-                                    </div>
-                                </a>
-                                <div class="like-comm pull-right">
-                                    <a href="#"><i class="fa fa-comment-o"></i>77</a>
-                                    <a href="#"><i class="fa fa-heart-o"></i>12</a>
-                                </div>
-                            </figcaption>
-                        </figure>
-
-                        <div class="blog-content">
-                            <h3><a href="single-blog.html">Myra Hindley and her lover, Ian Brady, plotted and</a></h3>
-                            <p>This is a big project of our company, we are happy to completed this type projec which
-                                are
-                                get world famous award</p>
-                            <a href="single-blog.html" class="btn btn-brand">More</a>
-                        </div>
-                    </article>
-                </div>
-                <!--== Single Blog Post End ==-->
-
-                <!--== Single Blog Post start ==-->
-                <div class="col-lg-4 col-md-6">
-                    <article class="single-blog-post">
-                        <figure class="blog-thumb">
-                            <div class="blog-thumbnail">
-                                <img src="http://placehold.it/700x352" alt="Blog" class="img-fluid">
-                            </div>
-                            <figcaption class="blog-meta clearfix">
-                                <a href="single-blog.html" class="author">
-                                    <div class="author-pic">
-                                        <img src="http://placehold.it/200x200" alt="Author">
-                                    </div>
-                                    <div class="author-info">
-                                        <h5>Aileen Wuornos</h5>
-                                        <p>3 mins Ago</p>
-                                    </div>
-                                </a>
-                                <div class="like-comm pull-right">
-                                    <a href="#"><i class="fa fa-comment-o"></i>77</a>
-                                    <a href="#"><i class="fa fa-heart-o"></i>12</a>
-                                </div>
-                            </figcaption>
-                        </figure>
-
-                        <div class="blog-content">
-                            <h3><a href="single-blog.html">Nurse with a private practice, racked up dozens of
-                                    victims</a></h3>
-                            <p>This is a big project of our company, we are happy to completed this type projec which
-                                are
-                                get world famous award</p>
-                            <a href="single-blog.html" class="btn btn-brand">More</a>
-                        </div>
-                    </article>
-                </div>
+                        </article>
+                    </div>
+                @endfor
                 <!--== Single Blog Post End ==-->
             </div>
             <!--== Blog Content Wrapper ==-->
