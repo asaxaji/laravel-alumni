81663ecba4de594a9c7f7d598043257f04dda359
Risqi Romadhoni <risqir57@gmail.com>
2021-01-28 08:16:45 -0500

    Fixed popup anouncement

--- app/Http/Livewire/Popup/Anouncement.php
+++ app/Http/Livewire/Popup/Anouncement.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Livewire\Popup;
 
+use App\Models\PopupMessage;
 use Livewire\Component;
 
 class Anouncement extends Component
 {
+    public $popup;
+
+    public function mount()
+    {
+        $pm = PopupMessage::whereIsPublish(true)->get();
+        $this->popup = [
+            'image' => ($pm->count() > 0) ? $pm->last()->image : null
+        ];
+    }
+
     public function render()
     {
         return view('livewire.popup.anouncement');
--- resources/views/livewire/popup/anouncement.blade.php
+++ resources/views/livewire/popup/anouncement.blade.php
@@ -1,3 +1,11 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+<div class="modal fade" id="popupModal" tabindex="-1" role="dialog" aria-labelledby="popupModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <button type="button" id="close-popup" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <img src="{{asset('storage/'.$popup['image'])}}"
+                class="img-fluid" />
+        </div>
+    </div>
 </div>
