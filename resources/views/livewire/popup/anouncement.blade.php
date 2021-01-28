affe28fa86eb372c0c935f18b6027749bdaed600
Risqi Romadhoni <risqir57@gmail.com>
2021-01-28 08:31:27 -0500

    popup with condition

--- resources/views/livewire/popup/anouncement.blade.php
+++ resources/views/livewire/popup/anouncement.blade.php
@@ -1,11 +1,13 @@
-<div class="modal fade" id="popupModal" tabindex="-1" role="dialog" aria-labelledby="popupModal" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <button type="button" id="close-popup" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            <img src="{{asset('storage/'.$popup['image'])}}"
-                class="img-fluid" />
+@if (!empty($popup['image']))
+    <div class="modal fade" id="popupModal" tabindex="-1" role="dialog" aria-labelledby="popupModal" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <button type="button" id="close-popup" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <img src="{{asset('storage/'.$popup['image'])}}"
+                    class="img-fluid" />
+            </div>
         </div>
-    </div>
-</div>
+    </div>    
+@endif
